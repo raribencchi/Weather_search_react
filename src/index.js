dbeab877ef33415ac574fd1e30fa2f1a6dbffa63
Rashmi Aribenchi <83005622+raribencchi@users.noreply.github.com>
2023-04-02 00:18:40 +0800

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,18 @@
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import axios from "axios";
+import WeatherSearch from "./WeatherSearch";
+
+import "./styles.css";
+
+const rootElement = document.getElementById("root");
+const root = createRoot(rootElement);
+
+root.render(
+  <div className="weatherSearch">
+    <h1>Weather App</h1>
+    <StrictMode>
+      <WeatherSearch />
+    </StrictMode>
+  </div>
+);
