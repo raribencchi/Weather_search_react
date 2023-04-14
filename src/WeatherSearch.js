2251dd74c223364ec2c2e514564b33087032b612
Rashmi Aribenchi <raribenchi@bunnings.com.au>
2023-04-15 00:30:39 +0800

    updated weathersearch

--- src/WeatherSearch.js
+++ src/WeatherSearch.js
@@ -1,61 +1,139 @@
-import React, { useState } from "react";
+import React from "react";
 import axios from "axios";
 
 export default function WeatherSearch() {
-  let [city, setCity] = useState(" ");
-  const [loaded, setLoaded] = useState(false);
-  let [weather, setWeather] = useState("");
-
-  function showWeather(response) {
-    setLoaded(true);
-    setWeather({
-      temperature: response.data.main.temp,
-      wind: response.data.wind.speed,
-      humidity: response.data.main.humidity,
-      icon: `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`,
-      description: response.data.weather[0].description
-      });
-
-   function SourceCodePath() {
-    <div>
-    <strong class="mycode">
-      <a href = "https://github.com/raribencchi/Weather_search_react" target="_blank">
-        Open Source Code: </a>  By Rashmi Aribenchi </strong>
-  </div>
-     }  
-    }
-
-  function handleSearch(event) {
-    event.preventDefault();
-    let url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=49e5da6325b173bba5c08dae7a5eba79&units=metric`;
-    axios.get(url).then(showWeather);
+    //Calculate date
+function formatDate(timeStamp) {
+  let date = new Date(timeStamp);
+  let hours = date.getHours();
+  let ampm = 'am';
+  if (hours > 12) {
+      hours = hours - 12;
+      ampm = 'pm';
   }
-  function updateCity(event) {
-    setCity(event.target.value);
+  if (hours <10) {
+      hours = '0' + hours;
   }
-  let form = (
-    <form onSubmit={handleSearch}>
-      <input type="search" placeholder="Enter a city.." onChange={updateCity} />
-      <button type="Submit">Search</button>
-    </form>
-  );
-
-  if (loaded) {
-    return (
-      <div>
-        {form}
-        <ul>
-          <li>Temperature: {Math.round(weather.temperature)}°C</li>
-          <li>Description: {weather.description}</li>
-          <li>Humidity: {weather.humidity}%</li>
-          <li>Wind: {weather.wind}km/h</li>
-          <li>
-            <img src={weather.icon} alt={weather.description} />
-          </li>
-        </ul>
-      </div>
-    );
-  } else {
-    return form;
+      let minutes = date.getMinutes();
+  if (minutes <10) {
+      minutes = '0' + minutes;
   }
+  let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+  let day = days[date.getDay()];
+  return day + ' ' + hours + ":" + minutes + ' ' + ampm;
 }
+
+// bring future week weather forecast to JS file and call it through function
+function formatDay(timestamp) {
+  let date = new Date (timestamp*1000);
+  let day = date.getDay();
+  let days = ["Sun","Mon", "Tue","Wed","Thu", "Fri", "Sat"]
+  return days[day];
+}
+
+function displayForecast(response) {
+  //console.log(response.data.daily);
+  let forecast = response.data.daily;
+  let forecastElement = document.querySelector("#forecast")
+  let forecastHTML = `<div className="row">`;
+  //concatenating the function
+  forecast.forEach(function(forecastDay, index) {
+  if (index < 6) {
+      forecastHTML = forecastHTML +
+      `<div className="col-2 P-sm-6 border text-center">
+      <div className="weather-forecast-date text-center">${formatDay (forecastDay.dt)}</div>
+      <img src="http://openweathermap.org/img/wn/${forecastDay.weather[0].icon}@2x.png" alt="" width="42"/>
+      <div className= "weather-forecst-temperatures">
+      <div className="weather-forecaste-temperature">
+      <span className="weather-forecaste-temperature-max">${Math.round(forecastDay.temp.max)}&#176</span>/<span className="weather-forecaste-temperature-min">${Math.round(forecastDay.temp.min)}&#176</span>  
+      </div>                   
+      </div>
+      </div>
+      `;}  
+  });
+  forecastHTML = forecastHTML + `</div>`;
+  forecastElement.innerHTML = forecastHTML;
+}
+
+function getForecast(coordinates) {
+  //console.log(coordinates);
+  let apiKey = "49e5da6325b173bba5c08dae7a5eba79";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
+  //console.log(apiUrl);
+  axios.get(apiUrl).then(displayForecast);
+}
+
+// define functions for each elements
+function displayTemperature(response) {
+  // console.log(response.data);
+  let cityElement = document.querySelector("#city");
+  cityElement.innerHTML = response.data.name;
+  let descriptionElement = document.querySelector("#description");
+  descriptionElement.innerHTML = response.data.weather[0].description;
+  celsiusTemperature = (response.data.main.temp);
+  let temperatureElement = document.querySelector("#temperature");
+  temperatureElement.innerHTML = Math.round (celsiusTemperature);
+  let humidityElement = document.querySelector("#humidity");
+  humidityElement.innerHTML = Math.round (response.data.main.humidity);
+  let windElement = document.querySelector("#wind");
+  windElement.innerHTML = Math.round (response.data.wind.speed);
+  let dateElement = document.querySelector("#date");
+  dateElement.innerHTML = formatDate (response.data.dt * 1000);
+  let iconElement = document.querySelector("#icon");
+  //instead of using innerhtml use setAttribute to change the feature of an element
+  iconElement.setAttribute("src", `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`);
+  //setAttribute for icon description
+  iconElement.setAttribute("alt", response.data.weather[0].description);
+  getForecast(response.data.coord);
+}
+
+// function to find city searched through API
+function search(city) {
+  //API call using api key
+  let apiKey = "49e5da6325b173bba5c08dae7a5eba79";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+  //console.log(apiUrl);
+  // connect to axios to interact with api 
+  axios.get(apiUrl).then(displayTemperature);
+}
+
+//Function to link search results
+function handleSearch(event) {
+  event.preventDefault();
+  let cityInputElement = document.querySelector("#city-input");
+  search(cityInputElement.value);
+  console.log(cityInputElement.value);
+}
+
+function displayFahrenheitTemperature(event) {
+  event.preventDefault();
+  let temperatureElement = document.querySelector("#temperature");
+  //to hide href link option when fahrenheit is clicked
+  celsiusLink.classNameList.remove("active");
+  fahrenheitLink.classNameList.add("active")
+  let fahrenheitTemperature = (celsiusTemperature * 9)/5 + 32;
+  temperatureElement.innerHTML = Math.round (fahrenheitTemperature);
+}
+
+function displaycelsiusTemperature(event) {
+  event.preventDefault();
+  //to hide href link option when celsius is clicked
+  celsiusLink.classNameList.add("active");
+  fahrenheitLink.classNameList.remove("active")
+  let temperatureElement = document.querySelector("#temperature");
+  temperatureElement.innerHTML = Math.round (celsiusTemperature);
+}
+
+let celsiusTemperature = null;
+search("Perth");
+//linking search results
+let form = document.querySelector("#search-form");
+form.addEventListener("submit", handleSearch);
+//event listener for temperatures
+let fahrenheitLink = document.querySelector("#fahrenheit-link");
+fahrenheitLink.addEventListener("click", displayFahrenheitTemperature);
+let celsiusLink = document.querySelector("#celsius-link");
+celsiusLink.addEventListener("click", displaycelsiusTemperature);
+   
+ }
+
--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import { createRoot } from "react-dom/client";
 import axios from "axios";
 import WeatherSearch from "./WeatherSearch";
 
+
 import "./styles.css";
 
 const rootElement = document.getElementById("root");
@@ -11,9 +12,32 @@ const root = createRoot(rootElement);
 root.render(
   <div className="weatherSearch">
     <h1>Weather App</h1>
+    <form id = "search-form" className = "mb-3">
+  <div className="row">
+    <div className="col-9">
+    <input type="search"
+    placeholder="Type a city..."
+    className="form-control"
+    id ="city-input"
+    autocomplete="off"/>
+    </div>
+    <div className="col-3">
+    <input type="submit"
+    value="Search"
+    className="btn btn-primary w-100 shadow-xl bg-gradient" />
+    </div>
+  </div>
+  </form>
+  <div className="overview">
+  <h1 id="city"></h1>
+<ul>
+<li> Last Updated: <span id = "date"> </span> </li>
+<li id="description"></li>
+</ul>
+</div>
     <StrictMode>
-  
-      <WeatherSearch />
-    </StrictMode>
+          <WeatherSearch />
+          </StrictMode>
+          
   </div>
 );
