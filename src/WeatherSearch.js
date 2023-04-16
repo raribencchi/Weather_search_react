d480d271763cb88bdb06992dac04bd31d49825cd
Rashmi Aribenchi <raribenchi@bunnings.com.au>
2023-04-16 13:47:39 +0800

    Update WeatherSearch.js file

--- src/WeatherSearch.js
+++ src/WeatherSearch.js
@@ -1,67 +1,141 @@
 import React, { useState } from "react";
 import axios from "axios";
 import ReactAnimatedWeather from "react-animated-weather";
-import FindDateNTemp from "./FindDateNTemp";
-import WeatherIcons from "./WeatherIcons";
+//import FindDateNTemp from "./FindDateNTemp";
+//import WeatherIcons from "./WeatherIcons";
 
 export default function WeatherSearch() {
-
-  <div class="container ">
-  <div class="weather-app-wrapper">
-    <div class="weather-app p-5">
-    <form id = "search-form" class = "mb-3">
-      <div class="row">
-        <div class="col-9">
-        <input type="search"
-        placeholder="Type a city..."
-        class="form-control"
-        id ="city-input"
-        autocomplete="off"/>
-        </div>
-        <div class="col-3">
-        <input type="submit"
-        value="Search"
-        class="btn btn-secondary w-100 shadow-xl bg-gradient" />
-        </div>
-      </div>
-      </form>
-      <div class="overview">
-        <h1 id="city"></h1>
-      <ul>
-      <li> Last Updated: <span id = "date"> </span> </li>
-      <li id="description"></li>
-      </ul>
-    </div>
-    <div class = "row">
-      <div class ="col-md-6">
-      <div class="clearfix weather-temperature" >
-        <img src = " "
-         alt ="Clear"
-         id = "icon"
-         class="float-left"/>
-        <div class="float-left">
-       <strong id="temperature"></strong> 
-       <span class="units"> <a href="#" id = "celsius-link" class="active">&#176C </a> | 
-       <a href="#" id="fahrenheit-link" class="text-dark text-decoration-none">&#176F</a></span>
-      </div>
-      </div>
-      </div>
-      <div class="col-md-6">
-        <ul>
-          <li>Humidity: <span id="humidity"></span> % </li>
-          <li>Wind: <span id = "wind"></span> Km/h</li>
-        </ul>
-      </div>
-      <div class="container text-center weather-forecast" id="forecast" >
-      <div class="d-fex justify-content-evenly p-6">
-        </div>
-      </div>
-      </div>
-    </div>
-  </div>
-    <strong class="mycode">
-      <a href = "https://github.com/raribencchi/Weather_search_react" target="_blank">
-        Open Source Code: </a>  By Rashmi Aribenchi </strong>
-  </div>
+  //Calculate date
+function formatDate(timeStamp) {
+  let date = new Date(timeStamp);
+  let hours = date.getHours();
+  let ampm = 'am';
+  if (hours > 12) {
+      hours = hours - 12;
+      ampm = 'pm';
   }
+  if (hours <10) {
+      hours = '0' + hours;
+  }
+      let minutes = date.getMinutes();
+  if (minutes <10) {
+      minutes = '0' + minutes;
+  }
+  let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+  let day = days[date.getDay()];
+  return day + ' ' + hours + ":" + minutes + ' ' + ampm;
+}
 
+// bring future week weather forecast to JS file and call it through function
+function formatDay(timestamp) {
+  let date = new Date (timestamp*1000);
+  let day = date.getDay();
+  let days = ["Sun","Mon", "Tue","Wed","Thu", "Fri", "Sat"]
+  return days[day];
+}
+
+function displayForecast(response) {
+  //console.log(response.data.daily);
+  let forecast = response.data.daily;
+  let forecastElement = document.querySelector("#forecast")
+  let forecastHTML = `<div class="row">`;
+  //concatenating the function
+  forecast.forEach(function(forecastDay, index) {
+  if (index < 6) {
+      forecastHTML = forecastHTML +
+      <div class="col-2 P-sm-6 border text-center">
+      <div class="weather-forecast-date text-center">${formatDay (forecastDay.dt)}</div>
+      <img src="http://openweathermap.org/img/wn/${forecastDay.weather[0].icon}@2x.png" alt="" width="42"/>
+      <div class= "weather-forecst-temperatures">
+      <div class="weather-forecaste-temperature">
+      <span class="weather-forecaste-temperature-max">${Math.round(forecastDay.temp.max)}&#176</span>/<span class="weather-forecaste-temperature-min">${Math.round(forecastDay.temp.min)}&#176</span>  
+      </div>                   
+      </div>
+      </div>
+      ;}  
+  });
+  forecastHTML = forecastHTML + `</div>`;
+  forecastElement.innerHTML = forecastHTML;
+}
+
+function getForecast(coordinates) {
+  //console.log(coordinates);
+  let apiKey = "49e5da6325b173bba5c08dae7a5eba79";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
+  //console.log(apiUrl);
+  axios.get(apiUrl).then(displayForecast);
+}
+
+// define functions for each elements
+function displayTemperature(response) {
+  // console.log(response.data);
+  let cityElement = document.querySelector("#city");
+  cityElement.innerHTML = response.data.name;
+  let descriptionElement = document.querySelector("#description");
+  descriptionElement.innerHTML = response.data.weather[0].description;
+  celsiusTemperature = (response.data.main.temp);
+  let temperatureElement = document.querySelector("#temperature");
+  temperatureElement.innerHTML = Math.round (celsiusTemperature);
+  let humidityElement = document.querySelector("#humidity");
+  humidityElement.innerHTML = Math.round (response.data.main.humidity);
+  let windElement = document.querySelector("#wind");
+  windElement.innerHTML = Math.round (response.data.wind.speed);
+  let dateElement = document.querySelector("#date");
+  dateElement.innerHTML = formatDate (response.data.dt * 1000);
+  let iconElement = document.querySelector("#icon");
+  //instead of using innerhtml use setAttribute to change the feature of an element
+  iconElement.setAttribute("src", `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`);
+  //setAttribute for icon description
+  iconElement.setAttribute("alt", response.data.weather[0].description);
+  getForecast(response.data.coord);
+}
+
+// function to find city searched through API
+function search(city) {
+  //API call using api key
+  let apiKey = "49e5da6325b173bba5c08dae7a5eba79";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+  //console.log(apiUrl);
+  // connect to axios to interact with api 
+  axios.get(apiUrl).then(displayTemperature);
+}
+
+//Function to link search results
+function handleSearch(event) {
+  event.preventDefault();
+  let cityInputElement = document.querySelector("#city-input");
+  search(cityInputElement.value);
+  console.log(cityInputElement.value);
+}
+
+function displayFahrenheitTemperature(event) {
+  event.preventDefault();
+  let temperatureElement = document.querySelector("#temperature");
+  //to hide href link option when fahrenheit is clicked
+  celsiusLink.classList.remove("active");
+  fahrenheitLink.classList.add("active")
+  let fahrenheitTemperature = (celsiusTemperature * 9)/5 + 32;
+  temperatureElement.innerHTML = Math.round (fahrenheitTemperature);
+}
+
+function displaycelsiusTemperature(event) {
+  event.preventDefault();
+  //to hide href link option when celsius is clicked
+  celsiusLink.classList.add("active");
+  fahrenheitLink.classList.remove("active")
+  let temperatureElement = document.querySelector("#temperature");
+  temperatureElement.innerHTML = Math.round (celsiusTemperature);
+}
+
+let celsiusTemperature = null;
+search("Perth");
+//linking search results
+let form = document.querySelector("#search-form");
+form.addEventListener("submit", handleSearch);
+//event listener for temperatures
+let fahrenheitLink = document.querySelector("#fahrenheit-link");
+fahrenheitLink.addEventListener("click", displayFahrenheitTemperature);
+let celsiusLink = document.querySelector("#celsius-link");
+celsiusLink.addEventListener("click", displaycelsiusTemperature);
+
+} 
